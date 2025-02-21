4055d5894d4c070975c0da71c88d125dcbb72b44
luckstraw <aviles.jamesluck.ecc.bscs@gmail.com>
2025-02-22 01:49:53 +0800

    TextNode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+
+    print(TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev"))
+
+if __name__ == "__main__":
+    main()
